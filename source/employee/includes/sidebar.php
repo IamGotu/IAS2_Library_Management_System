2ce104525de42d096b7d30815a6cbdb90774f5af
Mark John <markjohnjopia1@gmail.com>
2025-06-28 00:04:31 +0800

    sidebar updated

--- source/employee/includes/sidebar.php
+++ source/employee/includes/sidebar.php
@@ -8,9 +8,21 @@
         <li class="nav-item mb-2">
             <a class="nav-link text-white" href="../employee/dashboard.php"><i class="fas fa-home me-2"></i>Dashboard</a>
         </li>
-        <li class="nav-item mb-2">
-            <a class="nav-link text-white" href="../employee/manage_users.php"><i class="fas fa-users me-2"></i>Manage Users</a>
-        </li>
+
+        <?php
+        // Define which roles can access "Manage Users"
+        $allowed_roles = [1, 2, 6]; // Library Admin, Librarian, IT Personnel
+
+        if (in_array($_SESSION['role_id'], $allowed_roles)) {
+            echo '
+            <li class="nav-item mb-2">
+                <a class="nav-link text-white" href="../employee/manage_users.php">
+                    <i class="fas fa-users me-2"></i>Manage Users
+                </a>
+            </li>';
+        }
+        ?>
+
         <li class="nav-item">
             <a class="nav-link text-white" href="../employee/materials_management.php"><i class="fa-solid fa-book-open-reader me-2"></i>Manage Materials</a>
         </li>
