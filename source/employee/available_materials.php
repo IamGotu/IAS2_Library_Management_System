4cdd27814dd1a6217211dade7f79e0e3a65dcde8
Mark John <markjohnjopia1@gmail.com>
2025-06-27 20:11:07 +0800

    bootstrap

--- source/employee/available_materials.php
+++ source/employee/available_materials.php
@@ -119,6 +119,7 @@ foreach ($transactions as $tr) {
     <meta charset="UTF-8" />
     <title>Available Materials</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
 <div class="d-flex">
