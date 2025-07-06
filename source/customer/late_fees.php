69d43343c296bc879f4e43f169e2e05d932d207a
Mark John <markjohnjopia1@gmail.com>
2025-07-06 21:05:01 +0800

    updated late_fees.php

--- source/customer/late_fees.php
+++ source/customer/late_fees.php
@@ -2,7 +2,7 @@
 session_start();
 include '../../config/db_conn.php';
 
-if (!isset($_SESSION['user_id'])) {
+if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'customer') {
     header("Location: ../login.php");
     exit;
 }
